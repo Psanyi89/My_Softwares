44a057488297aa7894a499c9ac504149704702fc
Psanyi89 <pleskosandor@gmail.com>
2019-04-28 16:47:31 +0200

    Getting User Data

---
+++ TimCoRetailManager/TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+begin
+set nocount on;
+	SELECT * from [dbo].[User] where Id=@Id;
+	end
---
+++ TimCoRetailManager/TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public async Task<List<UserModel>> GetById()
+        {
+            string id = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+          return await data.GetUserById(id);
+           
+        }
+    }
+}
